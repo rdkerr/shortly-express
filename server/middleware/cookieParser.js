41acb55b6b26d686cd326f649b2d6fef82e9567b
rdkerr <rogerdkerr@gmail.com>
2019-01-28 18:29:26 -0700

    Implemented cookieParser

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,16 @@
 const parseCookies = (req, res, next) => {
+  if (!req.headers.cookie) {
+    next();
+    return;
+  }
+  var array = req.headers.cookie.split('; ');
+  var parsedCookies = array.reduce((result, cookie) => {
+    var parsedCookie = cookie.split('=');
+    result[parsedCookie[0]] = parsedCookie[1];
+    return result;
+  }, {});
+  req.cookies = parsedCookies;
+  next();
 };
 
 module.exports = parseCookies;
