132db15ee78a3c699f83ca6a669c3c3577a98291
rdkerr <rogerdkerr@gmail.com>
2019-01-28 21:09:40 -0700

    Added framework for session

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,21 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  // Access the  parse cookies on the request
+  // Assign an object to the session property on the request
+  // containing relevant user information
+  // Look up user data related to that session
+  console.log(req);
+  console.log('RESPONSEPPPPPPPPPPPPPPPPP',res);
+  // User has not been associated with user
+  if (Object.keys(req.cookies).length === 0) {
+    var newHash = models.Sessions.create();
+    req.session = { hash: newHash };
+    res.cookies = { shortlyid: { value: newHash } };
+  } else {
+    var currentHash = req.cookies.shortlyid.value;
+  }
+  next();
 };
 
 /************************************************************/
